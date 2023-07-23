7ea20c38f851e157dbabcb844842d27866fb8270
Ibrahima Diallo <ibrahimadiallo@Ibrahimas-MBP.ht.home>
2023-07-23 17:15:37 -0400

    few files updated

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const api_routes = require('/Users/ibrahimadiallo/Desktop/Expresschallenge/Note-Taker/routes/api-routes.js')
 const html_routes = require('/Users/ibrahimadiallo/Desktop/Expresschallenge/Note-Taker/routes/html-routes.js')
 const PORT = process.env.PORT || 3001;
-// dynamically set the port
+// This is dynamically set the port
 const app = express();
 
-// Express middleware will always run the operation in the order from top to bottom "order matters"
+// This Express middleware will always run the operation in the order from top to bottom "order matters"
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -13,5 +13,5 @@ app.use(html_routes)
 app.use(api_routes)
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`your server is running on this  http://localhost:${PORT}`);
 });
